2e0c05a81b427cca0d62f532cdf764841527b72b
Jagadeesh <jagadeeshthegeek@gmail.com>
2021-01-01 23:33:23 +0530

    cu

--- src/modules/app/components/AppIcon.js
+++ src/modules/app/components/AppIcon.js
@@ -3,8 +3,5 @@ import { iconMap } from '../icons';
 
 export default function AppIcon({ icon, ...rest }) {
   const Icon = iconMap[icon];
-  console.log('AppIcon', { icon, Icon });
-  //   return <div>{icon}</div>;
-
   return Icon && <Icon {...rest} />;
 }
